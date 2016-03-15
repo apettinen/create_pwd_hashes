89cf6d6781d745421768ebd2bd2d96e99b6fe0eb
Antti Pettinen <antti.pettinen@tut.fi>
2016-03-15 11:43:10 +0200

    Commit by: AP - changed the program to use command line arguments, removed interactive prompts - added sensible default values

--- createPbkdf2PwdHash.py
+++ createPbkdf2PwdHash.py
@@ -10,19 +10,38 @@ import argparse
 import binascii
 import hashlib
 
-parser = argparse.ArgumentParser(description='Interactively create PBKDF2 passwords for user creation with Puppet/Chef for OS X 10.8 onwards. The created password will have a salt of 32-bit random string and will use SHA-512 digest, with a derived length of 128.')
+parser = argparse.ArgumentParser(description='Create PBKDF2 passwords for user creation with Puppet/Chef for OS X 10.8 and later. The created password will have a salt of a random string with defined length and will use defined digest, with a desired derived length.')
+parser.add_argument('Password', help="Password to create the hash from")
+parser.add_argument('-sl','--StringLength', help="Length of the random string (default 32)", type=int, default=32, nargs=1)
+parser.add_argument('-i','--iterations', help="Number of iterations, min. 25000", type=int, default=25000, nargs=1)
+parser.add_argument('-d', '--digest', help="Digest to be used. Defaults to sha512", default='sha512', nargs=1, choices=['sha512', 'sha384', 'sha256'])
+parser.add_argument('-dl', '--DerivedLength', type=int, default=128, help="Desired derived key lenght, default 128", nargs=1)
+parser.add_argument('-v','--verbose', action='count', help="Increase the verbosity")
+#parser.add_argument
 args = parser.parse_args()
 
-desiredPassword = raw_input('Please enter the desired password: ')
-iterations = 0
-while iterations < 25000:
-    try:
-        iterations = int(raw_input('Please provide the amount of iterations (min 25000): '))
-    except ValueError:
-        iterations = 25000
-passwordSalt = os.urandom(32)
+if args.iterations < 25000:
+    args.iterations = 25000
+    #
+    # try:
+    #     args.iterations = int(raw_input('Please provide the amount of iterations (min 25000): '))
+    # except ValueError:
+    #     iterations = 25000
+
+
+if args.verbose:
+    print "PBKDF2 hash will be created with the following arguments"
+    print 'Password:', args.Password
+    print 'Salt string length:', args.StringLength
+    print 'Number of iterations:', args.iterations
+    print 'Defined digest:', args.digest
+    print 'Derived Lenght:', args.DerivedLength
+
+passwordSalt = os.urandom(args.StringLength)
+passwordHash = hashlib.pbkdf2_hmac(args.digest, args.Password, passwordSalt, args.iterations, args.DerivedLength)
+
 hexPasswordSalt = binascii.hexlify(passwordSalt)
-passwordHash = hashlib.pbkdf2_hmac('sha512', desiredPassword, passwordSalt, iterations, 128)
 hexPasswordHash = binascii.hexlify(passwordHash)
+
 print 'Password hash: ', hexPasswordHash
 print 'Salt hash: ', hexPasswordSalt
